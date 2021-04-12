ab1cf3b0047823c3f1b9f7e44734e45e413243f5
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-04-12 15:54:11 -0700

    project-02 assignment

--- app/src/main/java/edu/wallawalla/cs/pricqu/travelapp/MainActivity.java
+++ app/src/main/java/edu/wallawalla/cs/pricqu/travelapp/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.wallawalla.cs.pricqu.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View mTravelRadio = findViewById(R.id.travel_radio);
+
+/*        public void onRadioButtonClicked(android.view.View view) {
+            // Which radio button was selected?
+            switch (view.getId()) {
+                case R.id.radio_yes:
+                    findViewById(R.id.radio_output.setText("Button1 has been chosen");
+                    // "Local" selected
+                    break;
+                case R.id.radio_no:
+                    // "International" selected
+                    break;
+            }
+        }*/
+
     }
 }
