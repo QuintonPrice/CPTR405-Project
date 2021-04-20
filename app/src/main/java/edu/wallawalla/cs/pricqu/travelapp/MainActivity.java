ce17f45d6a1122b9f5a9662d62484deb2ef421f4
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-04-19 21:42:33 -0700

    made working click button

--- app/src/main/java/edu/wallawalla/cs/pricqu/travelapp/MainActivity.java
+++ app/src/main/java/edu/wallawalla/cs/pricqu/travelapp/MainActivity.java
@@ -2,19 +2,44 @@ package edu.wallawalla.cs.pricqu.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         View mTravelRadio = findViewById(R.id.travel_radio);
+        final Button findDestinationsButton = findViewById(R.id.find_destinations);
+        findDestinationsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText numberInputEditText=findViewById(R.id.how_many_travelers);
+                String temp = numberInputEditText.getText().toString();
+                int value = 0;
+                if (!"".equals(temp)) {
+                    value=Integer.parseInt(temp);
+                }
+                if (value == 0) {
+                    Toast.makeText(getApplicationContext(), "Cannot have 0 travelers!", Toast.LENGTH_SHORT).show();
+                }
+                else if (value > 0) {
+                    Toast.makeText(getApplicationContext(), "Valid number of travelers!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 /*        public void onRadioButtonClicked(android.view.View view) {
             // Which radio button was selected?
             switch (view.getId()) {
@@ -30,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // for saving state values
     @Override
     public void onSaveInstanceState(Bundle outState) {
         // insert items that need to be saved. currently don't have any
@@ -40,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // for getting state values
     @Override
     public void onRestoreInstanceState(Bundle savedState) {
         final EditText textBox = (EditText) findViewById(R.id.how_many_travelers);
